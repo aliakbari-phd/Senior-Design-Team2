0c02fe4a70ba80c528c60d5411aaee17fb824e7d
benj626 <benj626@tamu.edu>
2016-04-12 16:09:33 -0500

    Update figure saving functions

--- BenJohnston/Matlab/DataDisplayCorrection.m
+++ BenJohnston/Matlab/DataDisplayCorrection.m
@@ -1,7 +1,7 @@
 %NOTE:
 %Need to import GyroZ and Ltime columns from Bapgui
 
-filename = '2 hour stationary unplugged 2.txt';
+filename = '200 Hz 15 sec 01.txt';
 delimiterIn = '\t';
 headerlinesIn = 1;
 A = importdata(filename, delimiterIn, headerlinesIn);
@@ -18,14 +18,14 @@ Ltime = (A.data(:,13));
 t = transpose((Ltime-Ltime(1))./1000);     %relative to start time, ms to s
 
 %remove first one percent of data
-%onepercent = round(0.01*length(a_velocity));
+onepercent = round(0.01*length(a_velocity));
 %a_velocity(1:onepercent,:)=[];
 %t(1:onepercent,:)=[];
 
 
 %*******low pass filter*****
 x_filter = designfilt('lowpassiir','FilterOrder',3,...
-            'PassbandFrequency',100,'PassbandRipple',0.5,...
+            'PassbandFrequency',10e3,'PassbandRipple',0.5,...
             'SampleRate',200e3);
 a_velocity = filter(x_filter,a_velocity);
 
@@ -40,8 +40,8 @@ c_velocity = transpose(polyval(p1,t));      %best fit line of velocity data
 mean_x = mean(a_velocity(:,1));
 mean_y = mean(a_velocity(:,2));
 mean_z = mean(a_velocity(:,3));
-%a_velocity(:,1)=a_velocity(:,1)-mean_x;
-%a_velocity(:,2)=a_velocity(:,2)-mean_y;
+a_velocity(:,1)=a_velocity(:,1)-mean_x;
+a_velocity(:,2)=a_velocity(:,2)-mean_y;
 %a_velocity(:,3)=a_velocity(:,3)-mean_z;
 
 %zero testing
@@ -61,20 +61,35 @@ a_distance = cumtrapz(t,a_velocity);     % vel to distance
 position(:,1) = cosd(a_distance(:,3))+sind(a_distance(:,2));
 position(:,2) = -1+cosd(a_distance(:,1))+sind(a_distance(:,3));
 position(:,3) = -1+cosd(a_distance(:,2))+sind(a_distance(:,1));
-%{
+
 %plotting
 set(gcf,'color','white')
 subplot(2,1,1)
 plot3(position(:,1),position(:,2),position(:,3))
-xlabel('x'),ylabel('y'),zlabel('z')
+title('3-D Linear Position based on Relative Angular Velocity')
+xlabel('x Position'),ylabel('y Position'),zlabel('z Position')
+zlim([-1 1])
 grid on
+savefig('3d_grid.fig')
+
 
 subplot(2,1,2)
-%}
 plot(t,a_velocity(:,1))
+title('x Axis Angular Velocity')
 ylabel('Angular Velocity (deg/s)'),xlabel('Time (s)')
-ylim([-1.7 -1.4])
+%ylim([-.1 .1])
 
+%results
+abs_distance = cumtrapz(t,abs(a_velocity));
+
+result_duration = t(end);                   %report duration of test
+result_Fs = length(a_velocity)/t(end);       %Fs = sampling frequency
+result_xdistance = abs(abs_distance(end,1));      %report final distance
+result_ydistance = abs(abs_distance(end,2));
+result_zdistance = abs(abs_distance(end,3));  
+result_drift = abs(a_velocity(end,1)-a_velocity(onepercent,1));
+result_bfdrift = abs(c_velocity(end)-c_velocity(1)); %difference in begin to end of best fit velocity
+result_mean_vel = abs(mean(a_velocity(:,3)));   %report mean velocity
 
 
 %subplot(3,1,3)
@@ -101,9 +116,7 @@ for n=2:length(t)
 end;
 
 %results:
-result_drift = abs(v1(end)-v1(1)); %difference in begin to end of best fit velocity
-result_duration = t(end);
-result_Fs = length(velocity)/t(end);       %Fs = sampling frequency
+
 result_corrected_distance = dist_corrected(end);     %report final corrected distance
 result_distance = distance(end);    %report final distance
 result_mean_vel = abs(mean(velocity));   %report mean velocity
