80b65c89e99db22771ce709fcbefcfbbbcc848db
Alex S. Dubois <ghoz94@tamu.edu>
2016-04-15 19:53:16 -0500

    Updated Integrated Sensor Code and Added software flow charts for DDI and Data Analysis

---
+++ AlexDubois/BodyBasics-WPF-IntegratedSensorsUpdated/DataAnalysis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class DataAnalysis
+{
+    List<Int16> angle;
+    List<Int16> angularAccel;
+    List<Int16> angularJerk;
+    SensorData correctedData;
+
+    public DataAnalysis(SensorData cData)
+	{
+        angle = default(List<Int16>);
+        angularAccel = default(List<Int16>);
+        angularJerk = default(List<Int16>);
+        correctedData = cData;
+    }
+
+    private void CalcAngle()
+    {
+        /*for(int i = 0; i < correctedData.GetWearableData().Count; i ++)
+        {
+            List<Int16> data = correctedData.GetWearableData().FindIndex(i);
+            for(int j = 0; j < correctedData.GetWearableData().FindIndex(i).Count; j++)
+            {
+
+            }
+        }
+        */
+    }
+
+    private void CalcAngularAccel()
+    {
+
+    }
+
+    private void CalcAnglularJerk()
+    {
+
+    }
+}
---
+++ AlexDubois/BodyBasics-WPF-IntegratedSensorsUpdated/SensorData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SensorData
+{
+    //Each list contains set of a specific joint's data, first element denotes the Enum
+    List<List<float>> kinectData;
+    //Each list connected to a wearable sensor
+    List<List<Int16>> wearableData;
+
+
+    public SensorData(List<List<float>> kinectJoints, List<List<Int16>> wearable)
+	{
+        kinectData = kinectJoints;
+        wearableData = wearable;
+    }
+
+    public List<List<float>> GetKinectData()
+    {
+        return kinectData;
+    }
+
+    public List<List<Int16>> GetWearableData()
+    {
+        return wearableData;
+    }
+}
