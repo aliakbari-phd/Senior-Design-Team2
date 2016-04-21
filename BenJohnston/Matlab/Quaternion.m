a122ad3aeb14deeb60659c0280a3e638d64ccce9
benj626 <benj626@tamu.edu>
2016-04-21 11:13:20 -0500

    quaternion corrections added

--- BenJohnston/Matlab/Quaternion.m
+++ BenJohnston/Matlab/Quaternion.m
@@ -4,66 +4,58 @@ filename = 'Swing 23D 15 sec.txt';
 delimiterIn = '\t';
 headerlinesIn = 1;
 A = importdata(filename, delimiterIn, headerlinesIn);
-acceleration(:,1)=(A.data(:,1))./4096;
-acceleration(:,2)=(A.data(:,2))./4096;
-acceleration(:,3)=(A.data(:,3))./4096;
-velocity(:,1)=(A.data(:,4))./(32.75);
-velocity(:,2)=(A.data(:,5))./(32.75);
-velocity(:,3)=(A.data(:,6))./(32.75);
+acc(:,1)=(A.data(:,1))./4096;
+acc(:,2)=(A.data(:,2))./4096;
+acc(:,3)=(A.data(:,3))./4096;
+gyro(:,1)=(A.data(:,4))./(32.75);
+gyro(:,2)=(A.data(:,5))./(32.75);
+gyro(:,3)=(A.data(:,6))./(32.75);
 mag(:,1)=(A.data(:,7));
 mag(:,2)=(A.data(:,8));
 mag(:,3)=(A.data(:,9));
 Ltime(1,:) = (A.data(:,13))./1000;      %convert to milliseconds
 
+%*******low pass filter*****
+x_filter = designfilt('lowpassiir','FilterOrder',3,...
+            'PassbandFrequency',10e3,'PassbandRipple',0.5,...
+            'SampleRate',200e3);
+gyro = filter(x_filter,gyro);
+
+%correction factor (eliminate offset "constant")
+mean_x = mean(gyro(:,1));
+mean_y = mean(gyro(:,2));
+mean_z = mean(gyro(:,3));
+gyro(:,1)=gyro(:,1)-mean_x;
+gyro(:,2)=gyro(:,2)-mean_y;
+gyro(:,3)=gyro(:,3)-mean_z;
+
 T = (Ltime(end)-Ltime(1))/length(Ltime);     %find average period
 t = (1:length(Ltime)).*T;       %create time vector based on average period
 
-aa = velocity.*T;     %axis angle
+aa = gyro.*T;     %create axis matrix
 
-for i=1:length(aa)
+
+
+for i=1:length(aa)  %create angle matrix
     angle(i,:,1) = norm(aa(i,:));
     aa(i,:) = aa(i,:)./norm(aa(i,:));
-    acceleration(i,:) = acceleration(i,:)./norm(acceleration(i,:));
+    acc(i,:) = acc(i,:)./norm(acc(i,:));
     mag(i,:) = mag(i,:)./norm(mag(i,:));
 end
 
-q(:,1) = cosd(angle./2);     %quaternion
+q(:,1) = cosd(angle./2);     %create quaternion matrix
 q(:,2) = aa(:,1).*sind(angle./2);
 q(:,3) = aa(:,2).*sind(angle./2);
 q(:,4) = aa(:,3).*sind(angle./2);
 
-%{
-subplot(3,1,1)
-hold on
-plot (t, velocity(:,1), 'r')
-plot (t, velocity(:,2), 'g')
-plot (t, velocity(:,3), 'b')
-hold off
-
-subplot(3,1,2)
-hold on
-plot (t, mag(:,1), 'r')
-plot (t, mag(:,2), 'g')
-plot (t, mag(:,3), 'b')
-hold off
-legend('x', 'y', 'z')
-
-subplot(3,1,3)
-hold on
-plot (t, acceleration(:,1), 'r')
-plot (t, acceleration(:,2), 'g')
-plot (t, acceleration(:,3), 'b')
-hold off
-legend('x', 'y', 'z')
-%}
-
-point(1,:) = [0 acceleration(1,:)];
+qpoint(1,:) = [0 acc(1,:)];
 for i=2:length(q)
-    point(i,:) = quaternProd(q(i,:),quaternProd(point(i-1,:),quaternConj(q(1,:))));
+    qpoint(i,:) = quaternProd(q(i,:),quaternProd(qpoint(i-1,:),quaternConj(q(1,:))));
 end;
 
-euler = quatern2euler(quaternConj(point));
-figure('Name', 'Euler Angles');
+euler = (quatern2euler(quaternConj(qpoint))).*(180/pi);
+
+%subplot(2,1,1);
 hold on;
 plot(t, euler(:,1), 'r');
 plot(t, euler(:,2), 'g');
@@ -73,3 +65,11 @@ xlabel('Time (s)');
 ylabel('Angle (deg)');
 legend('\phi', '\theta', '\psi');
 hold off;
+
+%{
+x=zeros(size(gyro));
+subplot(2,1,2);
+quiver3(x(:,1),x(:,2),x(:,3),q(:,2),q(:,3),q(:,4));
+%xlim([-1 1]), ylim([-1 1]), zlim([-1 1]);
+xlabel('x'), ylabel('y'), zlabel('z')
+%}
