d8a9053807d7f4cadf3004cadedcc56c36f1e36f
burnsks89 <burnsks89@tamu.edu>
2016-09-19 15:04:06 -0500

    Matlab Code Edits

--- BennyChan/BodyBasics-WPF-IntegratedSensorsUpdated/SD01/DataAcquisitionPlotting.m
+++ BennyChan/BodyBasics-WPF-IntegratedSensorsUpdated/SD01/DataAcquisitionPlotting.m
@@ -1,7 +1,7 @@
 delimiterIn = ' ';
 headerlinesIn = 0;
 
-filenameA = 'spinebase.txt';
+filenameA = 'spinebaseVT1.txt';
 A = importdata(filenameA, delimiterIn, headerlinesIn);
 SpineBaseX = str2double(A.textdata(:,1));
 SpineBaseY = str2double(A.textdata(:,2));
@@ -9,6 +9,32 @@ SpineBaseZ = str2double(A.textdata(:,3));
 SpineBaseX = SpineBaseX.*100;
 SpineBaseY = SpineBaseY.*100;
 SpineBaseZ = SpineBaseZ.*100;
+
+if (SpineBaseX < 0)
+    SpineBaseX = SpineBaseX + SpineBaseX(1,1);
+elseif (SpineBaseX > 0)
+    SpineBaseX = SpineBaseX - SpineBaseX(1,1);
+else
+    SpineBaseX = SpineBaseX;
+end
+
+if (SpineBaseY < 0)
+    SpineBaseY = SpineBaseY - SpineBaseY(1,1);
+elseif (SpineBaseY > 0)
+    SpineBaseY = SpineBaseY - SpineBaseY(1,1);
+else
+    SpineBaseY = SpineBaseY;
+end
+
+if (SpineBaseZ < 0)
+    SpineBaseZ = SpineBaseZ + SpineBaseZ(1,1);
+elseif (SpineBaseZ > 0)
+    SpineBaseZ = SpineBaseZ - SpineBaseZ(1,1);
+else
+    SpineBaseZ = SpineBaseZ;
+end
+
+
 t = A.data(:,1);
 TestDuration = t-t(1);
 TestDuration = TestDuration./1000;
@@ -35,116 +61,116 @@ subplot(4, 1, 3), plot(TestDuration, SpineBaseZ), ylabel('PositionZ (cm)');
 subplot(4, 1, 4), plot(TestDuration, SpineBaseTracked), ylabel('IsTracked');
 xlabel('seconds');
 
-filenameB = 'spinemid.txt';
-B = importdata(filenameB, delimiterIn, headerlinesIn);
-SpineMidX = str2double(B.textdata(:,1));
-SpineMidY = str2double(B.textdata(:,2));
-SpineMidZ = str2double(B.textdata(:,3));
-SpineMidX = SpineMidX.*100;
-SpineMidY = SpineMidY.*100;
-SpineMidZ = SpineMidZ.*100;
-y = 1:length(SpineMidX);
-TrackedMid = B.textdata(:,4);
-SpineMidTracked = zeros(sz);
-for n=y
-    if strcmp(TrackedMid(n),'Tracked');
-        SpineMidTracked(n) = 2;
-    elseif strcmp(TrackedMid(n),'Inferred');
-        SpineMidTracked(n) = 1;
-    else 
-        SpineMidTracked(n) = 0;
-    end
-end
-figure(2)
-set(gcf, 'color', 'white');
-subplot(4, 1, 1), plot(TestDuration, SpineMidX), ylabel('PositionX (cm)');
-title('SpineMid');
-subplot(4, 1, 2), plot(TestDuration, SpineMidY), ylabel('PositionY (cm)');
-subplot(4, 1, 3), plot(TestDuration, SpineMidZ), ylabel('PositionZ (cm)');
-subplot(4, 1, 4), plot(TestDuration, SpineMidTracked), ylabel('IsTracked');
-xlabel('seconds');
-
-filenameC = 'spineshoulder.txt';
-C = importdata(filenameC, delimiterIn, headerlinesIn);
-SpineShoulderX = str2double(C.textdata(:,1));
-SpineShoulderY = str2double(C.textdata(:,2));
-SpineShoulderZ = str2double(C.textdata(:,3));
-SpineShoulderX = SpineShoulderX.*100;
-SpineShoulderY = SpineShoulderY.*100;
-SpineShoulderZ = SpineShoulderZ.*100;
-z = 1:length(SpineShoulderX);
-TrackedShoulder = C.textdata(:,4);
-SpineShoulderTracked = zeros(sz);
-for n=z
-    if strcmp(TrackedShoulder(n),'Tracked');
-        SpineShoulderTracked(n) = 2;
-    elseif strcmp(TrackedShoulder(n),'Inferred');
-        SpineShoulderTracked(n) = 1;
-    else 
-        SpineShoulderTracked(n) = 0;
-    end
-end
-figure(3)
-set(gcf, 'color', 'white');
-subplot(4, 1, 1), plot(TestDuration, SpineShoulderX), ylabel('PositionX (cm)');
-title('SpineShoulder');
-subplot(4, 1, 2), plot(TestDuration, SpineShoulderY), ylabel('PositionY (cm)');
-subplot(4, 1, 3), plot(TestDuration, SpineShoulderZ), ylabel('PositionZ (cm)');
-subplot(4, 1, 4), plot(TestDuration, SpineShoulderTracked), ylabel('IsTracked');
-xlabel('seconds');
-
-filenameD = 'rightshoulder.txt';
-D = importdata(filenameD, delimiterIn, headerlinesIn);
-RightShoulderX = str2double(D.textdata(:,1));
-RightShoulderY = str2double(D.textdata(:,2));
-RightShoulderZ = str2double(D.textdata(:,3));
-RightShoulderX = RightShoulderX.*100;
-RightShoulderY = RightShoulderY.*100;
-RightShoulderZ = RightShoulderZ.*100;
-w = 1:length(RightShoulderX);
-TrackedRight = D.textdata(:,4);
-RightShoulderTracked = zeros(sz);
-for n=w
-    if strcmp(TrackedRight(n),'Tracked');
-        RightShoulderTracked(n) = 2;
-    elseif strcmp(TrackedRight(n),'Inferred');
-        RightShoulderTracked(n) = 1;
-    else 
-        RightShoulderTracked(n) = 0;
-    end
-end
-figure(4)
-set(gcf, 'color', 'white');
-subplot(4, 1, 1), plot(TestDuration, RightShoulderX), ylabel('PositionX (cm)');
-title('RightShoulder');
-subplot(4, 1, 2), plot(TestDuration, RightShoulderY), ylabel('PositionY (cm)');
-subplot(4, 1, 3), plot(TestDuration, RightShoulderZ), ylabel('PositionZ (cm)');
-subplot(4, 1, 4), plot(TestDuration, RightShoulderTracked), ylabel('IsTracked');
-xlabel('seconds');
-
-filenameE = 'flexangle.txt';
-E = importdata(filenameE, delimiterIn, headerlinesIn);
-flexAngle = E(:,1);
-v = 1:length(flexAngle);
-figure(6)
-set(gcf, 'color', 'white');
-subplot(2, 1, 1), plot(TestDuration, flexAngle), ylabel('Degrees');
-title('Flex Angle');
-xlabel('seconds');
-
-
-figure(5)
-set(gcf, 'color', 'white');
-plot3(SpineShoulderX, SpineShoulderZ, SpineShoulderY);
-xlabel('PositionX (cm)');
-ylabel('PositionZ (cm)');
-zlabel('PositionY (cm)');
-hold on;
-%plot(RightShoulderZ, RightShoulderY);
-%hold on;
-plot3(SpineMidX, SpineMidZ, SpineMidY);
-hold on;
-plot3(SpineBaseX, SpineBaseZ, SpineBaseY);
-legend('Spine Shoulder', 'Spine Mid', 'Spine Base');
-legend('boxoff')
-title('Session 1 Test');
+% filenameB = 'spinemid.txt';
+% B = importdata(filenameB, delimiterIn, headerlinesIn);
+% SpineMidX = str2double(B.textdata(:,1));
+% SpineMidY = str2double(B.textdata(:,2));
+% SpineMidZ = str2double(B.textdata(:,3));
+% SpineMidX = SpineMidX.*100;
+% SpineMidY = SpineMidY.*100;
+% SpineMidZ = SpineMidZ.*100;
+% y = 1:length(SpineMidX);
+% TrackedMid = B.textdata(:,4);
+% SpineMidTracked = zeros(sz);
+% for n=y
+%     if strcmp(TrackedMid(n),'Tracked');
+%         SpineMidTracked(n) = 2;
+%     elseif strcmp(TrackedMid(n),'Inferred');
+%         SpineMidTracked(n) = 1;
+%     else 
+%         SpineMidTracked(n) = 0;
+%     end
+% end
+% figure(2)
+% set(gcf, 'color', 'white');
+% subplot(4, 1, 1), plot(TestDuration, SpineMidX), ylabel('PositionX (cm)');
+% title('SpineMid');
+% subplot(4, 1, 2), plot(TestDuration, SpineMidY), ylabel('PositionY (cm)');
+% subplot(4, 1, 3), plot(TestDuration, SpineMidZ), ylabel('PositionZ (cm)');
+% subplot(4, 1, 4), plot(TestDuration, SpineMidTracked), ylabel('IsTracked');
+% xlabel('seconds');
+% 
+% filenameC = 'spineshoulder.txt';
+% C = importdata(filenameC, delimiterIn, headerlinesIn);
+% SpineShoulderX = str2double(C.textdata(:,1));
+% SpineShoulderY = str2double(C.textdata(:,2));
+% SpineShoulderZ = str2double(C.textdata(:,3));
+% SpineShoulderX = SpineShoulderX.*100;
+% SpineShoulderY = SpineShoulderY.*100;
+% SpineShoulderZ = SpineShoulderZ.*100;
+% z = 1:length(SpineShoulderX);
+% TrackedShoulder = C.textdata(:,4);
+% SpineShoulderTracked = zeros(sz);
+% for n=z
+%     if strcmp(TrackedShoulder(n),'Tracked');
+%         SpineShoulderTracked(n) = 2;
+%     elseif strcmp(TrackedShoulder(n),'Inferred');
+%         SpineShoulderTracked(n) = 1;
+%     else 
+%         SpineShoulderTracked(n) = 0;
+%     end
+% end
+% figure(3)
+% set(gcf, 'color', 'white');
+% subplot(4, 1, 1), plot(TestDuration, SpineShoulderX), ylabel('PositionX (cm)');
+% title('SpineShoulder');
+% subplot(4, 1, 2), plot(TestDuration, SpineShoulderY), ylabel('PositionY (cm)');
+% subplot(4, 1, 3), plot(TestDuration, SpineShoulderZ), ylabel('PositionZ (cm)');
+% subplot(4, 1, 4), plot(TestDuration, SpineShoulderTracked), ylabel('IsTracked');
+% xlabel('seconds');
+% 
+% filenameD = 'rightshoulder.txt';
+% D = importdata(filenameD, delimiterIn, headerlinesIn);
+% RightShoulderX = str2double(D.textdata(:,1));
+% RightShoulderY = str2double(D.textdata(:,2));
+% RightShoulderZ = str2double(D.textdata(:,3));
+% RightShoulderX = RightShoulderX.*100;
+% RightShoulderY = RightShoulderY.*100;
+% RightShoulderZ = RightShoulderZ.*100;
+% w = 1:length(RightShoulderX);
+% TrackedRight = D.textdata(:,4);
+% RightShoulderTracked = zeros(sz);
+% for n=w
+%     if strcmp(TrackedRight(n),'Tracked');
+%         RightShoulderTracked(n) = 2;
+%     elseif strcmp(TrackedRight(n),'Inferred');
+%         RightShoulderTracked(n) = 1;
+%     else 
+%         RightShoulderTracked(n) = 0;
+%     end
+% end
+% figure(4)
+% set(gcf, 'color', 'white');
+% subplot(4, 1, 1), plot(TestDuration, RightShoulderX), ylabel('PositionX (cm)');
+% title('RightShoulder');
+% subplot(4, 1, 2), plot(TestDuration, RightShoulderY), ylabel('PositionY (cm)');
+% subplot(4, 1, 3), plot(TestDuration, RightShoulderZ), ylabel('PositionZ (cm)');
+% subplot(4, 1, 4), plot(TestDuration, RightShoulderTracked), ylabel('IsTracked');
+% xlabel('seconds');
+% 
+% filenameE = 'flexangle.txt';
+% E = importdata(filenameE, delimiterIn, headerlinesIn);
+% flexAngle = E(:,1);
+% v = 1:length(flexAngle);
+% figure(6)
+% set(gcf, 'color', 'white');
+% subplot(2, 1, 1), plot(TestDuration, flexAngle), ylabel('Degrees');
+% title('Flex Angle');
+% xlabel('seconds');
+% 
+% 
+% figure(5)
+% set(gcf, 'color', 'white');
+% plot3(SpineShoulderX, SpineShoulderZ, SpineShoulderY);
+% xlabel('PositionX (cm)');
+% ylabel('PositionZ (cm)');
+% zlabel('PositionY (cm)');
+% hold on;
+% %plot(RightShoulderZ, RightShoulderY);
+% %hold on;
+% plot3(SpineMidX, SpineMidZ, SpineMidY);
+% hold on;
+% plot3(SpineBaseX, SpineBaseZ, SpineBaseY);
+% legend('Spine Shoulder', 'Spine Mid', 'Spine Base');
+% legend('boxoff')
+% title('Session 1 Test');
--- KevinBurns/MATLAB Code/ViconCal02Sep19.m
+++ KevinBurns/MATLAB Code/ViconCal02Sep19.m
@@ -1,6 +1,5 @@
 filename = 'Ben_Johnston Cal 02.csv';
-V_Base = xlsread(filename, 'C18:E3116');
-V_Upper = xlsread(filename, 'I12:Z4612');
+V_Base = xlsread(filename, 'C16:E3114');
 
 V_Base_Zeroed_X = V_Base(:,1)-V_Base(1,1);
 V_Base_Zeroed_Y = V_Base(:,2)-V_Base(1,2);
@@ -9,6 +8,13 @@ V_Base_Zeroed_Z = V_Base(:,3)-V_Base(1,3);
 V_Base_Zero(:,1) = V_Base_Zeroed_X;
 V_Base_Zero(:,2) = V_Base_Zeroed_Y;
 V_Base_Zero(:,3) = V_Base_Zeroed_Z;
+
+Captures = size(V_Base_Zeroed_X);
+ViconDuration = Captures(1,1);
+ViconDurationSec = ViconDuration/15;
+
+Vlin = linspace(1, ViconDuration, ViconDuration);
+VSpace = transpose(Vlin./ViconDurationSec);
 
 Maximum = max(abs(V_Base_Zero));
 
@@ -22,17 +28,146 @@ V_BaseNorm_Z = V_Base_Zeroed_Z./True_Max;
 
 % V_Base_Normalized = V_Base_Zeroed_X./V_Base_Zeroed.max;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+delimiterIn = ' ';
+headerlinesIn = 0;
+
+filenameA = 'spinebaseVT2.txt';
+A = importdata(filenameA, delimiterIn, headerlinesIn);
+SpineBaseX = str2double(A.textdata(:,1));
+SpineBaseY = str2double(A.textdata(:,2));
+SpineBaseZ = str2double(A.textdata(:,3));
+SpineBaseX = SpineBaseX.*100;
+SpineBaseY = SpineBaseY.*100;
+SpineBaseZ = SpineBaseZ.*100;
+
+SpineBaseX = SpineBaseX - SpineBaseX(1,1);
+SpineBaseY = SpineBaseY - SpineBaseY(1,1);
+SpineBaseZ = SpineBaseZ - SpineBaseZ(1,1);
+
+SBXMax = max(abs(SpineBaseX));
+SBYMax = max(abs(SpineBaseY));
+SBZMax = max(abs(SpineBaseZ));
+
+SpineBaseX = SpineBaseX./SBXMax;
+SpineBaseY = SpineBaseY./SBYMax;
+SpineBaseZ = SpineBaseZ./SBZMax;
+
+t = A.data(:,1);
+TestDuration = t-t(1);
+TestDuration = TestDuration./1000;
+x = 1:length(SpineBaseX);
+TrackedBase = A.textdata(:,4);
+sz = size(SpineBaseX);
+SpineBaseTracked = zeros(sz);
+for n=x
+    if strcmp(TrackedBase(n),'Tracked');
+        SpineBaseTracked(n) = 2;
+    elseif strcmp(TrackedBase(n),'Inferred');
+        SpineBaseTracked(n) = 1;
+    else 
+        SpineBaseTracked(n) = 0;
+    end
+end
+%TestDurationMovement = TestDuration(1: 151);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+filenameS1 = 'ViconTest21.txt';
+filenameS2 = 'ViconTest22.txt';
+delimiterIn = ' ';
+headerlinesIn = 1;
+S1 = importdata(filenameS1, delimiterIn, headerlinesIn);
+S2 = importdata(filenameS2, delimiterIn, headerlinesIn);
+
+GyroS1(:,1) = (S1.data(:,4))./32.75;
+GyroS1(:,2) = (S1.data(:,5))./32.75;
+GyroS1(:,3) = (S1.data(:,6))./32.75;
+GyroS2(:,1) = (S2.data(:,4))./32.75;
+GyroS2(:,2) = (S2.data(:,5))./32.75;
+GyroS2(:,3) = (S2.data(:,6))./32.75;
+kS1 = S1.data(:,10);
+kS2 = S2.data(:,10);
+TestDurationS1 = kS1-kS1(1);
+TestDurationS1 = transpose(TestDurationS1./1000);
+TestDurationS2 = kS2-kS2(1);
+TestDurationS2 = transpose(TestDurationS2./1000);
+
+%*******low pass filter*****
+x_filter = designfilt('lowpassiir','FilterOrder',3,...
+            'PassbandFrequency',10e3,'PassbandRipple',0.5,...
+            'SampleRate',200e3);
+GyroS1 = filter(x_filter,GyroS1);
+GyroS2 = filter(x_filter,GyroS2);
+
+%best fit code
+tS1(:,1) = transpose(TestDurationS1);
+tS1(:,2) = transpose(TestDurationS1);
+tS1(:,3) = transpose(TestDurationS1);
+p1 = polyfit(tS1,GyroS1,1);
+c_velocityS1 = transpose(polyval(p1,TestDurationS1));
+
+tS2(:,1) = transpose(TestDurationS2);
+tS2(:,2) = transpose(TestDurationS2);
+tS2(:,3) = transpose(TestDurationS2);
+p2 = polyfit(tS2,GyroS2,1);
+c_velocityS2 = transpose(polyval(p2,TestDurationS2));
+
+%differentiation and integration
+a_accelerationS1 = diff(GyroS1);             % vel to accel 
+a_accelerationS1 = [0,[1 3];a_accelerationS1];
+a_jerkS1 = diff(a_accelerationS1);             %accel to jerk
+a_jerkS1 = [0,[1 3];a_jerkS1];
+a_positionS1 = trapz(TestDurationS1,GyroS1);
+a_distanceS1 = cumtrapz(TestDurationS1,GyroS1);     % vel to distance
+
+a_accelerationS2 = diff(GyroS2);             % vel to accel 
+a_accelerationS2 = [0,[1 3];a_accelerationS2];
+a_jerkS2 = diff(a_accelerationS2);             %accel to jerk
+a_jerkS2 = [0,[1 3];a_jerkS1];
+a_positionS2 = trapz(TestDurationS2,GyroS2);
+a_distanceS2 = cumtrapz(TestDurationS2,GyroS2);     % vel to distance
+
+%angular distance to linear position
+positionS1(:,1) = -1+cosd(a_distanceS1(:,3))+sind(a_distanceS1(:,2));
+positionS1(:,2) = -1+cosd(a_distanceS1(:,1))+sind(a_distanceS1(:,3));
+positionS1(:,3) = -1+cosd(a_distanceS1(:,2))+sind(a_distanceS1(:,1));
+
+positionS2(:,1) = -1+cosd(a_distanceS2(:,3))+sind(a_distanceS2(:,2));
+positionS2(:,2) = -1+cosd(a_distanceS2(:,1))+sind(a_distanceS2(:,3));
+positionS2(:,3) = -1+cosd(a_distanceS2(:,2))+sind(a_distanceS2(:,1));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%PLOTTING HERE%%%%%%%%%%%%%%%%%%
+
+figure(1)
+set(gcf, 'color', 'white');
+
 subplot(3,1,1)
-plot(V_BaseNorm_X)
-title('Vicon X-Direction')
+plot(VSpace,V_BaseNorm_X,TestDurationS2, positionS2(:,1),TestDuration,SpineBaseZ)
+title('X Position')
 
 subplot(3,1,2)
-plot(V_BaseNorm_Y)
-title('Vicon Y-Direction')
+plot(VSpace,V_BaseNorm_Y,TestDurationS2, positionS2(:,2),TestDuration,SpineBaseX)
+title(' Y Position')
 ylabel('Normalized Position')
 ylim([-1 1])
 
 subplot(3,1,3)
-plot(V_BaseNorm_Z)
-title('Vicon Z-Direction')
-xlabel('Captures')
+plot(VSpace,V_BaseNorm_Z,TestDurationS2, positionS2(:,3),TestDuration,SpineBaseY)
+title('Z Position')
+xlabel('Time (s)')
+
+
+
+
+
+% subplot(4, 1, 1), plot(TestDuration, SpineBaseX), ylabel('PositionX (cm)');
+% title('SpineBase');
+% subplot(4, 1, 2), plot(TestDuration, SpineBaseY), ylabel('PositionY (cm)');
+% subplot(4, 1, 3), plot(TestDuration, SpineBaseZ), ylabel('PositionZ (cm)');
+ % subplot(4, 1, 4), plot(TestDuration, SpineBaseTracked), ylabel('IsTracked');
+% xlabel('seconds');
